d58e5215649e6c21da690e89a4b7c0a41f627924
Zakaria BARI <zakariabari213@gmail.com>
2023-03-15 18:31:39 +0100

    first commit

---
+++ Exercices.py
@@ -0,0 +1,77 @@
+    # 5. Ecrire un algorithme qui permet de calculer le périmètre et la surface d’un rectangle ?
+
+# L = float(input("Entrer la longueur du rectangle : "))
+# l = float(input("Entrer la largeur du rectangle : "))
+# Surf = L*l 
+# Perim = (L+l)*2
+# print("La surface du rectangle est :", Surf)
+# print("Le périmétre du rectangle est :", Perim)
+
+    # 6. Ecrire un algorithme permettant d’échanger les valeurs de deux variables A et B, et ce quel que soit leur contenu préalable. 
+
+# A = int(input("Entrer la valeur de A : "))
+# B = int(input("Entrer la valeur de B : "))
+# C = B
+# B = A 
+# A = C
+# print("La valeur de A sera :", A)
+# print("La valeur de B sera :", B)
+
+    # 7. Ecrire un algorithme qui permet de faire la permutation entre trois nombres entiers ?
+
+# X = int(input("Entrer la valeur de X : "))
+# Y = int(input("Entrer la valeur de Y : "))
+# Z = int(input("Entrer la valeur de Z : "))
+
+# print("Valeurs initiales : X =", X, ", Y =", Y, ", Z =", Z)
+
+# temp = X
+# X = Y
+# Y = temp
+
+# temp = Y
+# Y = Z
+# Z = temp
+
+# print("Nouvelles valeurs : X =", X, ", Y =", Y, ", Z =", Z)
+
+
+    # 8. A la fin d’année, pour chaque étudiant doit avoir deux notes : note écrite (ne) de coefficient 5 et noteorale (no) de coefficient 3. Ecrire un algorithme qui permet de calculer la moyenne et d’afficher l’un des résultats suivantes : « Admis » ou « Echoué »
+
+# NEcrite = float(input("Entrer la note écrite :"))*5
+# NOrale = float(input("Entrer la note orale :"))*3
+# Moyenne = (NEcrite + NOrale)/2
+
+# if Moyenne < 10:
+#     print("Echoué")
+# else:
+#     print("Admis")
+
+
+    # 9. Un magasin dispose de cinq produits :
+
+# ProdA = 5.00
+# ProdB = 2.50
+# ProdC = 3.00
+# ProdD = 10.00
+# ProdE = 7.00
+
+# X = int(input("Combien de X unités du produit A : "))*ProdA
+# Y = int(input("Combien de Y unités du produit B : "))*ProdB
+# Z = int(input("Combien de Z unités du produit C : "))*ProdC
+# T = int(input("Combien de T unités du produit D : "))*ProdD
+# U = int(input("Combien de U unités du produit E : "))*ProdE
+
+# ttva = 0.20
+
+# pht = (ProdA*X) + (ProdB*Y) + (ProdC*Z) + (ProdD*T) + (ProdE*U)
+
+# tva = pht * ttva
+
+# pttc = pht * tva 
+
+# print("Le prix hors taxe (PHT) est : ", pht)
+# print("Le taxe sur la valeur ajoutée (TVA) est : ", tva)
+# print("Le prix toutes taxes comprises (PTTC) est : ", pttc)
+
+
