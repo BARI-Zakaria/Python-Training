4c754531358c10dbaf92df102aace979efe7af5f
$ZakariaBari_Dev <93256333+BARI-Zakaria@users.noreply.github.com>
2023-04-19 00:45:21 +0000

    Updates

--- Models/osModel.py
+++ Models/osModel.py
@@ -1,11 +1,14 @@
 import os
+from datetime import datetime
 
         #   TO GET YOUR LOCAL LOCATION
 print(os.getcwd())
 
         #    TO CHANGE YOUR LOCATION
-patH = "C:\\Users\\Zakaria\\Desktop\\Python"
+patH = "C:\\Users\\Zakaria\\Desktop\\Python\\PythonModels"
 os.chdir(patH)
+
+paTH = "C:\\Users\\Zakaria\\Desktop\\Python"
 
         #   TO LIST THE CONTENT INSIDE A DIRECTORY
 print(os.listdir())
@@ -18,4 +21,29 @@ os.makedirs('Models1/OSModel/MMM')
 
 os.rmdir('Models1/OSModel/MMM')
 os.removedirs('Models1/OSModel')
+
+        #   TO RENAME A FILE/DIRECTORY
+
+os.rename("Models", "PythonModels")
+
+        #   TO GET STATS/DETAILS OF A FILE
+
+print(os.stat('osModel.py')) #GENERALLY
+print(os.stat('osModel.py').st_atime) #SPECIFIED
+print(os.stat('osModel.py').st_size) #SPECIFIED
+
+        #   TURN THE 'st_mtime' TO THE NORMAL FORMAT
+
+from datetime import datetime
+
+time = os.stat('osModel.py').st_mtime
+print(datetime.fromtimestamp(time))
+
+        #   GET THE DETAILS OF A DIREC/SUB-DIREC & FILES WITH 'os.walk()' METHOD
+
+for dirpath, dirnames, filenames in os.walk(paTH):
+        print('Current path :', dirpath)
+        print('Directories :', dirnames)
+        print('Files :', filenames)
+
 
