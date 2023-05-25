bbd067474b545508d625f4e5bd06557efbc5ff07
Zakaria BARI <zakariabari213@gmail.com>
2023-05-25 20:53:14 +0100

    Update

--- Models/osModel.py
+++ Models/osModel.py
@@ -46,23 +46,5 @@ for dirpath, dirnames, filenames in os.walk(paTH):
 
 
 
-import os
-import glob
-import shutil
 
-
-mySet=set()
-pathA = "C:\\Users\\Zakaria\\Desktop\\BB"
-test = glob.glob1(pathA, '*.*')
-# print(test)
-
-mySet.update(test)
-print(mySet)
-
-for dirpath, dirnames, filenames in os.walk(pathA):
-    for exten in filenames:
-        # if exten == ""
-        extenstion = os.path.splitext(exten)[1]
-        if extenstion in mySet:
-            shutil.copyfile()
 
