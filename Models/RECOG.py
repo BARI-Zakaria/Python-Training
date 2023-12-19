1d429c17f510aacc6e34fd8cf3710c46da435fad
Zakaria BARI <zakariabari213@gmail.com>
2023-12-19 23:16:39 +0100

    Task Automation

--- Models/RECOG.py
+++ Models/RECOG.py
@@ -70,5 +70,6 @@ while True:
             cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 0, 255), cv2.FILLED)
             cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
     
+    
     cv2.imshow("face recognition", img)
     cv2.waitKey(1)
